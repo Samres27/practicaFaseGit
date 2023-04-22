f9fd2d4cf40a52336f9dbc56c7ae68ecc4495f97
samres27 <yosoyam4@gmail.com>
2023-04-22 17:06:00 -0400

    BREAKING CHANGE: se crearon el archivo programa.java que contiene un pequeño programa calculadora

---
+++ carpetaExlusiva/programa.java
@@ -0,0 +1,27 @@
+public class contador{
+
+    public static void main(String[] abc){
+        int cant =sumame(3,2);
+        int result=dividir(cant,3); 
+        System.out.print(cant+"  "+result);
+
+    }
+    public static int sumame (int cant,int total){
+        for(int i=0;i<cant;i++){
+            total=total+1;
+        }
+        return total;
+
+    }
+    public  static int dividir(int total, int sobre){
+        int cant =0;
+        while(total>=sobre){
+            total-=sobre;
+            cant++;
+        }
+        return cant;
+
+    }
+
+
+}
